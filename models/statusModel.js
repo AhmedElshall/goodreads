7b34b6fb91f342ab1cc1788ff099f24de6247264
doaakhalf <khalf_doaa@yahoo.com>
2019-03-05 17:36:21 +0200

    displayBooks

--- models/statusModel.js
+++ models/statusModel.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const statusSchema = new mongoose.Schema({
-  status: { type: NotEmptyString, enum: ["1", "2", "3"] },
-  bookId: { type: mongoose.Schema.Types.ObjectId, ref: "books" },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "users" }
+  status:[{type: String, enum: ['1', '2', '3']}],
+  bookId: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }],
+  userId: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
 });
 
-const status = mongoose.model("status", reviewSchema);
+const status = mongoose.model("status", statusSchema);
 module.exports = status;
